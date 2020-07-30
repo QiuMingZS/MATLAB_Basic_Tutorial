3846d0c34ca20862a0b74c29d46be04be634f6b4
QiuMingZS <guanzhengnpu@gmail.com>
2020-07-30 15:51:04 +0800

    origin

--- movie_show.m
+++ movie_show.m
@@ -1,13 +1,18 @@
 figure
-s=0.2;x1=0;
-nframes=50;
-for k=1:nframes
-    x1=x1+s;
-    x=0:0.01:x1;
-    y=sin(x);
-    plot(x,y);
-    axis([0 2*pi -1 1]);
-    grid off;
-    M(k)=getframe;
-end                
-movie(M,3);
+x=0:0.01*pi:2*pi;
+y=sin(x);
+len_x = length(x);
+witerObj=VideoWriter('sin_video.avi');  
+open(writerObj);  
+for k=1:len_x
+    plot(x(1:k),y(1:k));
+    xlabel('t')
+    ylabel('sin(t)')
+    hold on 
+    plot(x(k), y(k), 'o')
+    axis([0 2*pi -1.1 1.1]);
+    frame = getframe;           
+    writeVideo(writerObj,frame); 
+    hold off
+end 
+close(writerObj);
