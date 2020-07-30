dcf6c047c865b451a2e7d6e640df104793dbbd4d
QiuMingZS <guanzhengnpu@gmail.com>
2020-07-31 07:37:38 +0800

    origin

--- Examples/dynamic_figure/movie_show.m
+++ Examples/dynamic_figure/movie_show.m
@@ -1,14 +1,14 @@
-clear
-x=0:0.01*pi:2*pi;
-y=sin(x);
+clear,clc
+x = 0:0.01*pi:2*pi;
+y = sin(x);
 len_x = length(x);
-writerObj=VideoWriter('sin_video.avi');  
+writerObj = VideoWriter('sin_video.avi');  
 open(writerObj);  
 figure
 for k=1:len_x
-    plot(x(1:k),y(1:k));
-    xlabel('t')
-    ylabel('sin(t)')
+    plot(x(1:k), y(1:k));
+    xlabel('x')
+    ylabel('sin(x)')
     hold on 
     plot(x(k), y(k), 'o')
     axis([0 2*pi -1.1 1.1]);
